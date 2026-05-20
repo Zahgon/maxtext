ebd108c49fa997e1701de57aab40856a9ee9a3f1
aireenmei <aireenmei@gmail.com>
2026-05-20 00:11:49 +0000

    Remove tf from post-training

--- src/maxtext/experimental/rl/grpo_trainer.py
+++ src/maxtext/experimental/rl/grpo_trainer.py
@@ -45,8 +45,6 @@ from typing import Sequence, Callable, Iterator
 
 from absl import app
 
-import tensorflow as tf
-
 import numpy as np
 
 import jax
@@ -932,9 +930,6 @@ def main(argv: Sequence[str]) -> None:
   """
   pathwaysutils.initialize()
   jax.config.update("jax_default_prng_impl", "unsafe_rbg")
-  # TF allocates extraneous GPU memory when using TFDS data
-  # this leads to CUDA OOMs. WAR for now is to hide GPUs from TF
-  tf.config.set_visible_devices([], "GPU")
   os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
   if "xla_tpu_spmd_rng_bit_generator_unsafe" not in os.environ.get("LIBTPU_INIT_ARGS", ""):
     os.environ["LIBTPU_INIT_ARGS"] = (
@@ -965,7 +960,6 @@ def main(argv: Sequence[str]) -> None:
   jax.config.update("jax_use_shardy_partitioner", config.shardy)
   max_utils.print_system_information()
   train_utils.validate_train_config(config)
-  os.environ["TFDS_DATA_DIR"] = config.dataset_path
   vertex_tensorboard_manager = VertexTensorboardManager()
   if config.use_vertex_tensorboard or os.environ.get("UPLOAD_DATA_TO_TENSORBOARD"):
     vertex_tensorboard_manager.configure_vertex_tensorboard(config)
